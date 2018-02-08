7bf101f8c589bda759afb1fe76f6e7ae6d4cda4a
tejainece <tejainece@gmail.com>
2018-02-08 19:56:39 +0100

    NumericSeries

--- lib/src/series/numeric.dart
+++ lib/src/series/numeric.dart
@@ -1,5 +1,19 @@
 part of grizzly.primitives.series;
 
-abstract class NumericSeries<LT, VT extends num> implements Series<LT, VT> {
+abstract class NumericSeries<LT, VT extends num>
+    implements NumericSeriesView<LT, VT>, Series<LT, VT> {}
+
+abstract class NumericSeriesView<LT, VT extends num>
+    implements SeriesView<LT, VT> {
+  VT get max;
+
+  VT get min;
+
   NumericSeries<LT, double> get log;
+
+  NumericSeries<LT, double> log10();
+
+  NumericSeries<LT, double> logN(num n);
+
+  NumericSeries<LT, double> get abs;
 }
